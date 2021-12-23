8c999600d76b3ec004825e291082f128e2711482
dina193 <bolta.dina@gmail.com>
2021-12-23 14:00:27 +0100

    adding changes

--- cypress/integration/pomAllGalleries.spec.js
+++ cypress/integration/pomAllGalleries.spec.js
@@ -5,24 +5,34 @@ import { myGalleries } from "../page_objects/myGalleries";
 import { authLogin } from "../page_objects/authLogin";
 import { createGallery } from "../page_objects/createGallery";
 import { header } from "../page_objects/header";
+import { viewGallery } from "../page_objects/viewGalleryPage";
 
 
-describe('POM all galleries', () => {
+describe('Test all galleries page', () => {
 
     let allData = {
         email: 'dina1@test.com',
         password: 'dinatest1',
         galleryTitle: 'sunflower',
         galleryDesc: 'description',
-        imageUrl: 'https://onlinejpgtools.com/images/examples-onlinejpgtools/sunflower.jpg'
+        imageUrl: 'https://onlinejpgtools.com/images/examples-onlinejpgtools/sunflower.jpg',
+        authorName: 'Dina Bolta',
+        authorFirstName: 'Dina',
+        authorLastName: 'Bolta'
     }
-    before('Login and create gallery', () => {
+    
+    beforeEach('Login with valid credentials', () => {
         cy.visit('/login');
         authLogin.login(allData.email, allData.password);
         cy.url().should('not.include', '/login');
-        header.createGalleryBtn.click();
+    });
+
+    it('Create gallery', () => {
+        header.createGalleryBtn.should('exist').click();
         cy.url().should('contains', '/create');
+        createGallery.createGalleryPageHeading.should('be.visible').and('include.text', 'Create Gallery');
         createGallery.createGalleryWithOneImg(allData.galleryTitle, allData.galleryDesc, allData.imageUrl);
+        cy.url().should('not.include', '/create');
     });
 
     it('Testing pagination', () => {
@@ -39,9 +49,9 @@ describe('POM all galleries', () => {
 
     it('Get gallery by clicking on the author', () => {
         allGalleries.allGalleriesPageHeading.should('be.visible').and('have.text', 'All Galleries');
-        allGalleries.getGalleryAuthor('Dina Bolta').click();
+        allGalleries.getGalleryAuthor(allData.authorName).click();
         cy.url().should('include', '/authors');
-        myGalleries.myGalleriesPageHeading.should('contain.text', 'Galleries of');
+        myGalleries.myGalleriesPageHeading.should('contain.text', `Galleries of ${allData.authorFirstName}  ${allData.authorLastName}`);
         allGalleries.inputField.should('exist');
         allGalleries.filterBtn.should('be.visible');
     });
@@ -52,8 +62,8 @@ describe('POM all galleries', () => {
         allGalleries.allGalleriesPageHeading.should('be.visible').and('have.text', 'All Galleries');
         allGalleries.getGalleryTitle(allData.galleryTitle).click();
         cy.url().should('contains', '/galleries');
-        myGalleries.myGalleriesPageHeading.should('include.text', allData.galleryTitle);
-        cy.get('h5').first().should('include.text', 'created by');
-        cy.get('h5').last().should('contain.text', 'Comments');
+        viewGallery.viewGalleryPageHeading.should('include.text', allData.galleryTitle);
+        viewGallery.viewGalleryPageSubheadings.first().should('include.text', 'created by');
+        viewGallery.viewGalleryPageSubheadings.last().should('contain.text', 'Comments');
     });
 });
